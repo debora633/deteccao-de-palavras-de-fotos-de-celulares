a79e3f1f25d9577f7db02dca95933f5d87383ca2
Heitor Rocha <heitorocha3@gmail.com>
2026-06-04 16:54:44 -0300

    Implementacao do pre processamento de imagens

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import cv2
-from fastapi import background
 import numpy as np
 import matplotlib.pyplot as plt
 import os
@@ -54,7 +53,7 @@ for filename in images:
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     blur = cv2.GaussianBlur(gray, (3, 3), 0)
 
-    # CLAHE
+    # CLAHE - MELHORA CONTRASTE
     clahe = cv2.createCLAHE(
         clipLimit=2.0,
         tileGridSize=(8, 8)
@@ -62,7 +61,7 @@ for filename in images:
 
     contrast = clahe.apply(blur)
 
-    # SHARPEN
+    # SHARPEN - REALCE DE BORDAS
     kernel = np.array([
     [0, -1, 0],
     [-1, 5,-1],
@@ -70,12 +69,12 @@ for filename in images:
     ])
 
     sharp = cv2.filter2D(contrast, -1, kernel)
-    background = cv2.GaussianBlur(gray, (101,101), 0)
 
-    norm = cv2.divide(gray, background, scale=255)
+    #CORREÇÃO ILUMINAÇÃO
+    background = cv2.GaussianBlur(sharp, (101,101), 0)
+    norm = cv2.divide(sharp, background, scale=255)
     
     # BINARIZAÇÃO ADAPTATIVA GAUSSIANA
-
     binary = cv2.adaptiveThreshold(
         norm,
         255,
@@ -85,12 +84,54 @@ for filename in images:
         10
     )
 
-    # GERAR NOME 
-    name, ext = os.path.splitext(filename) 
-    output_name = f"{name}_processada{ext}" 
-    output_path = os.path.join( OUTPUT_FOLDER, output_name ) 
+    #MORFOLOGIA
+    morph_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3,3))
+
+    # fecha falhas nas letras
+    closed = cv2.morphologyEx(binary, cv2.MORPH_CLOSE, morph_kernel, iterations=2)
+
+    # junta regiões próximas
+    dilated = cv2.dilate(closed, morph_kernel, iterations=1)
+
+    #CONTORNOS
+    contours, hierarchy = cv2.findContours(dilated, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+# cópia para desenhar caixas
+    result = image.copy()
+
+    #BOUNDING BOXES
+    for contour in contours:
+        x,y,w,h = cv2.boundingRect(contour)
+
+        # remove ruídos pequenos
+        if w > 20 and h > 10:
+            cv2.rectangle(result, (x,y), (x+w,y+h), (0,255,0), 2)
+
     # SALVAR
-    cv2.imwrite(output_path, binary)
-    print(f"Salva em: {output_path}")
+    name, ext = os.path.splitext(filename)
+    # imagem em escala de cinza
+    cv2.imwrite(os.path.join(OUTPUT_FOLDER, f"{name}_gray{ext}"),gray)
+
+    # imagem após CLAHE (contraste)
+    cv2.imwrite(os.path.join(OUTPUT_FOLDER,f"{name}_clahe{ext}"),contrast)
+
+    # imagem após sharpen (nitidez)
+    cv2.imwrite(os.path.join(OUTPUT_FOLDER,f"{name}_sharp{ext}"),sharp)
+
+    # imagem após morfologia
+    cv2.imwrite(os.path.join(OUTPUT_FOLDER,f"{name}_morph{ext}"),dilated)
+
+    # imagem binarizada
+    cv2.imwrite(os.path.join(OUTPUT_FOLDER,f"{name}_binary{ext}"),binary)
+
+    # imagem final com bounding boxes
+    cv2.imwrite(os.path.join(OUTPUT_FOLDER,f"{name}_final{ext}"),result)
+
+    # MOSTRAR RESULTADO
+    show("Original", image)
+    show("CLAHE - Contraste Melhorado", contrast)
+    show("Sharpen - Nitidez", sharp)
+    show("Imagem Binaria", binary)
+    show("Texto Detectado", result)
 
 print("Processamento concluído.")
