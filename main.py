7abafe3cf1c02f5377a7d90780d8ae2d048f8694
Daniel Ramos de Oliveira Tenório Bomfim <58080460+debora633@users.noreply.github.com>
2026-06-04 18:07:51 -0300

    rotacao das imagens

--- main.py
+++ main.py
@@ -1,14 +1,30 @@
 import cv2
 import numpy as np
+import matplotlib.pyplot as plt
 import os
+
 from jdeskew.estimator import get_angle
 from jdeskew.utility import rotate
 
 # PASTAS
 INPUT_FOLDER = "dataset/originais"
-OUTPUT_FOLDER = "dataset/resultados"
+OUTPUT_FOLDER = "dataset/processadas"
 
 os.makedirs(OUTPUT_FOLDER, exist_ok=True)
+
+def show(title, image, cmap='gray'):
+
+    plt.figure(figsize=(10, 7))
+    plt.title(title)
+
+    if len(image.shape) == 2:
+        plt.imshow(image, cmap=cmap)
+    else:
+        plt.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+
+    plt.axis('off')
+    plt.show()
+
 valid_extensions = (".jpg", ".jpeg", ".png")
 
 images = [
@@ -25,17 +41,19 @@ for filename in images:
         print(f"Erro ao carregar: {filename}")
         continue
 
-    # UPSCALE DA IMAGEM
-    scale = 3 
+    # UPSCALE
+    scale = 3
     image = cv2.resize(image, None, fx=scale, fy=scale, interpolation=cv2.INTER_CUBIC)
 
     print(f"Processando: {filename}")
 
-    # DESKEW
+    # =========================
+    # DESKEW (ROTAÇÃO AUTOMÁTICA)
+    # =========================
     angle = get_angle(image)
     image = rotate(image, angle)
 
-    # CINZA
+    # ESCALA DE CINZA
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     blur = cv2.GaussianBlur(gray, (5, 5), 0)
 
@@ -49,6 +67,7 @@ for filename in images:
         [-1, 5, -1],
         [0, -1, 0]
     ])
+
     sharp = cv2.filter2D(contrast, -1, kernel)
 
     # ILUMINAÇÃO
@@ -65,51 +84,35 @@ for filename in images:
         10
     )
 
-    #MORFOLOGIA
-    morph_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3,3))
-
-    # fecha falhas nas letras
-    closed = cv2.morphologyEx(binary, cv2.MORPH_CLOSE, morph_kernel, iterations=2)
-
-    # junta regiões próximas
+    # MORFOLOGIA
+    morph_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (5, 5))
+    closed = cv2.morphologyEx(binary, cv2.MORPH_CLOSE, morph_kernel, iterations=3)
     dilated = cv2.dilate(closed, morph_kernel, iterations=1)
 
     # CONTORNOS
     contours, _ = cv2.findContours(dilated, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-# cópia para desenhar caixas
     result = image.copy()
 
-    for c in contours:
-        x, y, w, h = cv2.boundingRect(c)
+    for contour in contours:
+        x, y, w, h = cv2.boundingRect(contour)
+
         if w > 20 and h > 10:
             cv2.rectangle(result, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
     # SALVAR
     name, ext = os.path.splitext(filename)
-    # imagem em escala de cinza
-    cv2.imwrite(os.path.join(OUTPUT_FOLDER, f"{name}_gray{ext}"),gray)
 
-    # imagem após CLAHE (contraste)
-    cv2.imwrite(os.path.join(OUTPUT_FOLDER,f"{name}_clahe{ext}"),contrast)
+    cv2.imwrite(os.path.join(OUTPUT_FOLDER, f"{name}_upscale{ext}"), image)
+    cv2.imwrite(os.path.join(OUTPUT_FOLDER, f"{name}_gray{ext}"), gray)
+    cv2.imwrite(os.path.join(OUTPUT_FOLDER, f"{name}_clahe{ext}"), contrast)
+    cv2.imwrite(os.path.join(OUTPUT_FOLDER, f"{name}_sharp{ext}"), sharp)
+    cv2.imwrite(os.path.join(OUTPUT_FOLDER, f"{name}_morph{ext}"), dilated)
+    cv2.imwrite(os.path.join(OUTPUT_FOLDER, f"{name}_binary{ext}"), binary)
+    cv2.imwrite(os.path.join(OUTPUT_FOLDER, f"{name}_final{ext}"), result)
 
-    # imagem após sharpen (nitidez)
-    cv2.imwrite(os.path.join(OUTPUT_FOLDER,f"{name}_sharp{ext}"),sharp)
-
-    # imagem após morfologia
-    cv2.imwrite(os.path.join(OUTPUT_FOLDER,f"{name}_morph{ext}"),dilated)
-
-    # imagem binarizada
-    cv2.imwrite(os.path.join(OUTPUT_FOLDER,f"{name}_binary{ext}"),binary)
-
-    # imagem final com bounding boxes
-    cv2.imwrite(os.path.join(OUTPUT_FOLDER,f"{name}_final{ext}"),result)
-
-    # MOSTRAR RESULTADO
-    show("Original", image)
-    show("CLAHE - Contraste Melhorado", contrast)
-    show("Sharpen - Nitidez", sharp)
+    # MOSTRAR
+    show("Original (Deskew aplicado)", image)
     show("Imagem Binaria", binary)
-    show("Texto Detectado", result)
 
 print("Processamento concluído.")
